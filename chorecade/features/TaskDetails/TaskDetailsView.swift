9a3d88c66970b5dc90e17a388f97ec2443904df0
Gabriel Kowaleski <kowa@MacBook-Pro-de-Kowa.local>
2025-06-23 15:00:14 -0300

    Adding scrollView

--- chorecade/features/TaskDetails/TaskDetailsView.swift
+++ chorecade/features/TaskDetails/TaskDetailsView.swift
@@ -9,36 +9,6 @@ import UIKit
 
 class TaskDetailsView: UIView {
     // MARK: - Components
-//    let imageNames = ["before", "after"]
-//
-//    lazy var taskImageSlider: UIPageViewController = {
-//        let pageVC = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
-//        pageVC.dataSource = self
-//        return pageVC
-//    }()
-//    
-//    let imageBefore: UIImageView = {
-//        let imageView = UIImageView(image: UIImage(named: "before") ?? UIImage(named: "defaultImage"))
-//        imageView.translatesAutoresizingMaskIntoConstraints = false
-//        imageView.clipsToBounds = true
-//        imageView.contentMode = .scaleAspectFill
-//        return imageView
-//    }()
-//    
-//    let imageAfter: UIImageView = {
-//        let imageView = UIImageView(image: UIImage(named: "after") ?? UIImage(named: "defaultImage"))
-//        imageView.translatesAutoresizingMaskIntoConstraints = false
-//        imageView.clipsToBounds = true
-//        imageView.contentMode = .scaleAspectFill
-//        return imageView
-//    }()
-
-    lazy var sliderContainer: UIPageControl = {
-        let pageControl = UIPageControl()
-        pageControl.numberOfPages = 2
-        return pageControl
-    }()
-    
     lazy var taskTitleLabel = Components.getLabel(content: "Default Task", font: Fonts.titleConcludedTask)
     
     lazy var authorImageView = UIImageView()
@@ -52,7 +22,7 @@ class TaskDetailsView: UIView {
     lazy var commentTextField = UITextField()
 
     lazy var contentStack: UIStackView = {
-       let stackView = UIStackView(arrangedSubviews: [sliderContainer ,taskTitleLabel])
+       let stackView = UIStackView(arrangedSubviews: [taskTitleLabel])
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
         stackView.spacing = 16
@@ -73,10 +43,6 @@ class TaskDetailsView: UIView {
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func viewDidLayoutSubviews() {
-        
     }
 }
 
@@ -100,18 +66,5 @@ extension TaskDetailsView: ViewCodeProtocol {
             contentStack.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
             contentStack.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
         ])
-    }
-}
-
-
-extension TaskDetailsView: UIPageViewControllerDataSource {
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        guard let index = imageViewControllers.firstIndex(of: viewController), index > 0 else { return nil }
-        return imageViewControllers[index - 1]
-    }
-
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        guard let index = imageViewControllers.firstIndex(of: viewController), index < imageViewControllers.count - 1 else { return nil }
-        return imageViewControllers[index + 1]
     }
 }
--- chorecade/features/TaskDetails/TaskDetailsViewController.swift
+++ chorecade/features/TaskDetails/TaskDetailsViewController.swift
@@ -8,6 +8,17 @@
 import UIKit
 
 class TaskDetailsViewController: UIViewController {
+    // MARK: - Components
+    lazy var scrollView = UIScrollView()
+    
+    lazy var pageControl: UIPageControl = {
+        let pageControl = UIPageControl()
+        pageControl.translatesAutoresizingMaskIntoConstraints = false
+        pageControl.numberOfPages = 2
+        pageControl.backgroundColor = .primaryPurple300
+        return pageControl
+    }()
+    
     // MARK: - View
     lazy var taskDetailsView: TaskDetailsView = {
         let view = TaskDetailsView()
@@ -18,22 +29,47 @@ class TaskDetailsViewController: UIViewController {
     // MARK: - Functions
     override func viewDidLoad() {
         super.viewDidLoad()
+        pageControl.addTarget(self, action: #selector(pageControlDidChange(_:)), for: .valueChanged)
+        scrollView.backgroundColor = .systemRed
         setup()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        pageControl.frame = CGRect(x: 10, y: view.frame.height - 100, width: view.frame.width - 20, height: 70)
+        scrollView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height - 100)
+        
+        if scrollView.subviews.count == 2 {
+            configureScrollView()
+        }
+    }
+    
+    @objc func pageControlDidChange(_ sender: UIPageControl) {
+        let current = sender.currentPage
+        scrollView.setContentOffset(CGPoint(x: view.frame.width * CGFloat(current), y: 0), animated: true)
+    }
+    
+    func configureScrollView() {
+        scrollView.contentSize = CGSize(width: view.frame.width * 2, height: view.frame.height)
+        scrollView.isPagingEnabled = true
+        let colors: [UIColor] = [.systemBlue, .systemGreen]
+        
+        for i in 0..<2 {
+            let page = UIView(frame: CGRect(x: view.frame.width * CGFloat(i), y: 0, width: view.frame.width, height: view.frame.height))
+            page.backgroundColor = colors[i]
+            scrollView.addSubview(page)
+        }
     }
 }
 
 // MARK: - Extensions
 extension TaskDetailsViewController: ViewCodeProtocol {
     func addSubviews() {
-        view.addSubview(taskDetailsView)
+        view.addSubview(scrollView)
+        view.addSubview(pageControl)
     }
     
     func setupConstraints() {
-        NSLayoutConstraint.activate([
-            taskDetailsView.topAnchor.constraint(equalTo: view.topAnchor),
-            taskDetailsView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            taskDetailsView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            taskDetailsView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-        ])
+        
     }
 }
